a9ecd8fa61d390ebdb48b60bc3c52c68475765c8
pierre <pierrebigaud44@orange.fr>
2019-10-05 23:20:42 +0200

    Auto stash before rebase of "origin/master"

---
+++ Assets/Script/NiveauManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NiveauManager : MonoBehaviour
+{
+    /// <summary>
+    /// this script has the function of modyfying the difficulty of the game throughout days
+    /// 
+    /// it is a list of a class name Days, who apply variables, to the game.
+    /// 
+    /// </summary>
+    /// 
+    public class Day
+    {
+        public float TimeDays;
+
+    }
+}
