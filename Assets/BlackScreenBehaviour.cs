62e6bf8c4d187090e4a8b0093b6a8b9fc3b7942b
Guybrrush <corentin.brule@free.fr>
2019-10-16 22:12:03 +0200

    Added black screen to magnifying glass

---
+++ Assets/BlackScreenBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BlackScreenBehaviour : MonoBehaviour
+{
+    public MagnifyingGlassScript mgScript;
+
+    void OnMouseDown()
+    {
+        if (mgScript.isOn)
+        {
+            mgScript.useMagnifyingGlass();
+        }
+    }
+}
